8c7978782b00113a395ce607783db0e4f7d59cdd
Muhammad Aryan <160228293+MuhammadAriyan@users.noreply.github.com>
2024-04-23 04:40:22 -0700

    ADDED FILES

---
+++ words.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+import chalk from "chalk";
+import inquirer from "inquirer";
+console.log(chalk.bgMagenta("``WORD`COUNTER``"));
+const input = await inquirer.prompt({
+    type: "input",
+    name: "a",
+    message: chalk.yellow("ENTER YOUR WORD"),
+    validate(ans) {
+        if (ans) {
+            return true;
+        }
+        return chalk.red('PLEASE ENTER VALID INPUT');
+    }
+});
+console.log(chalk.green(`YOUR SENTENCE : ${input.a} `));
+let B = input.a.trim().replace(/\s+/g, ' ');
+let c = B.split(' ');
+console.log(chalk.blue(`IT GOT ${c.length} WORDS `));
+let a = `jidhuagdyeedgiidgqi hdouqwheowqheoiqw iuueiowueiowue                e`;
+console.log(a.split(`.`), a);
